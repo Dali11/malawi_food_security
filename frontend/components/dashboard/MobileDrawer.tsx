1853f2cbb72431aa49e0465e0fc225e7585fbc6f
Dali11 <kamphanidali@gmail.com>
2026-05-19 12:43:27 +0200

    fix: mobile drawer

--- frontend/components/dashboard/MobileDrawer.tsx
+++ frontend/components/dashboard/MobileDrawer.tsx
@@ -21,7 +21,7 @@ export function MobileDrawer({
 
   // Reset when district changes
   useEffect(() => {
-    if (district) setDrawerHeight("half")
+    if (district) setDrawerHeight("full")
     else setDrawerHeight("peek")
   }, [district])
 
